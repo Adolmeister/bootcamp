ea5f3342993aad25bda72466fe87c3265c30cd42
Adolmeister <adrianko4646@gmail.com>
2024-06-26 00:34:57 +0200

    added counter with button

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -3,6 +3,32 @@
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-    Siema
+    Siema!
+    <br/>
+    {{ licznik }}
+    <br/>
+    <button @click="increase">podbij</button>
+    <br/>
+    <button @click="decrease">zmniejsz</button>
+
   </main>
 </template>
+
+<script>
+export default{
+  data(){
+    return{
+      licznik: 0
+    }
+  },
+  methods:{
+    increase(){
+      this.licznik++
+    },
+    decrease(){
+      this.licznik--
+    }
+  }
+}
+
+</script>
